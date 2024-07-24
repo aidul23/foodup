e9145363ad4dff431275afd32ddb6307a8d8716e
Md. Aidul Islam <aidul5613@gmail.com>
2024-07-24 21:28:50 +0300

    updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,8 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:3000"],
+    origin: ["https://zippy-swan-6f36d1.netlify.app"],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   })
 );
 
