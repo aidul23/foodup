9cad9f0f66a2cca406fe2372ab6a38a52ce5052e
Md. Aidul Islam <aidul5613@gmail.com>
2024-07-24 20:34:48 +0300

    up

--- backend/src/server.js
+++ backend/src/server.js
@@ -29,7 +29,7 @@ app.use(`${process.env.BASE_URL}/orders`, orderRouter);
 
 // const publicFolder = path.join(__dirname,'public');
 
-app.use(express.static(publicFolder));
+// app.use(express.static(publicFolder));
 
 // app.get('*', (req,res) => {
 //   const indexFilePath = path.join(publicFolder,'index.html');
